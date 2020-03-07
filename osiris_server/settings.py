418870614c5ffa51b1429473497566da7f425ae6
Kalid Ahamed <admkhalid@zoho.com>
2020-03-08 01:33:09 +0530

    other stuff

--- osiris_server/settings.py
+++ osiris_server/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'jj&1belokw-9e)xszacd40h9mqaavt5$h213(%859qig3b46=e'
+SECRET_KEY = os.environ.get('OSIRIS_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
