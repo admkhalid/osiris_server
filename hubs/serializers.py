d56018089bedf926cea8db0aad48a37dff2ef842
Kalid Ahamed <admkhalid@zoho.com>
2020-03-10 23:35:07 +0530

    reorganized the code; added names for some urls

--- hubs/serializers.py
+++ hubs/serializers.py
@@ -1,13 +1,20 @@
 from rest_framework import serializers
 from .models import Merchant, Product, Hub
 
-class MerchantSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True, style = {'input_type': 'password'}) #FOR USER CREATION PURPOSE
-    # products = serializers.PrimaryKeyRelatedField(many=True, queryset=Product.objects.all())
+class HubSerializer(serializers.HyperlinkedModelSerializer):
+    url = serializers.HyperlinkedIdentityField(view_name='hub-detail', format='html')
 
     class Meta:
+        model = Hub
+        fields = ['hub_id', 'url', 'x_co_ord', 'y_co_ord', 'area', 'area_code']
+
+class MerchantSerializer(serializers.HyperlinkedModelSerializer):
+    password = serializers.CharField(write_only=True, style = {'input_type': 'password'}) #FOR USER CREATION PURPOSE
+    products = serializers.PrimaryKeyRelatedField(many=True, queryset=Product.objects.all())
+    # products = serializers.HyperlinkedRelatedField(many=True, view_name='product-detail', read_only=True)
+    class Meta:
         model = Merchant
-        fields = ['first_name', 'last_name', 'username', 'email', 'password', 'phone', 'hub']
+        fields = ['first_name', 'last_name', 'username', 'email', 'password', 'phone', 'hub', 'products']
     
     def create(self, validated_data):
         merchant = super(MerchantSerializer, self).create(validated_data)
@@ -15,15 +22,9 @@ class MerchantSerializer(serializers.ModelSerializer):
         merchant.save()
         return merchant
 
-
-class HubSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Hub
-        fields = ['hub_id', 'x_co_ord', 'y_co_ord', 'area', 'area_code']
-
-class ProductSerializer(serializers.ModelSerializer):
+class ProductSerializer(serializers.HyperlinkedModelSerializer):
     merchant = serializers.ReadOnlyField(source='merchant.username')
-
+    # url = serializers.HyperlinkedIdentityField(view_name='product-detail')
     class Meta:
         model = Product
         fields = ['id', 'merchant', 'name', 'family', 'avail_quantity']
--- hubs/urls.py
+++ hubs/urls.py
@@ -7,9 +7,9 @@ from .views import (
 urlpatterns = [
     path('', HubListView.as_view()),
     path('create-merch/', MerchantCreateView.as_view()),
-    path('<str:pk>/', HubDetailView.as_view()),
+    path('<str:pk>/', HubDetailView.as_view(), name='hub-detail'),
     path('<str:hub_id>/merch/', MerchantListView.as_view()),
     path('<str:hub_id>/products/', ProductListView.as_view()),
-    path('<str:hub_id>/products/<int:pk>/', ProductDetailView.as_view()),
+    path('<str:hub_id>/products/<int:pk>/', ProductDetailView.as_view(), name='product-detail'),
     path('<str:hub_id>/products/create/', ProductCreateView.as_view()),
 ]
