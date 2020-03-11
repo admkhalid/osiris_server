e155e9e40c6399f863203670cabc20bf57bdd43e
Kalid Ahamed <admkhalid@zoho.com>
2020-03-11 20:38:49 +0530

    just registered the Product model in admin panel

--- hubs/admin.py
+++ hubs/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Hub, Merchant
+from .models import Hub, Merchant, Product
 
 # Register your models here.
 admin.site.register(Hub)
 admin.site.register(Merchant)
+admin.site.register(Product)
