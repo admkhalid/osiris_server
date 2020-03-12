70d72888d28e30331af9ef32e9575be5ae7b8c16
Kalid Ahamed <admkhalid@zoho.com>
2020-03-13 00:43:20 +0530

    tokens created for merchant registration (signals)

--- hubs/models.py
+++ hubs/models.py
@@ -1,5 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.conf import settings
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from rest_framework.authtoken.models import Token
+
 
 # Create your models here.
 class Hub(models.Model):
@@ -30,3 +35,9 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name + ' ' + self.family + ' - ' + str(self.avail_quantity) + ' from ' + self.merchant.__str__()
+
+
+@receiver(post_save, sender=Merchant)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
