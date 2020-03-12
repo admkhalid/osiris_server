12dc0e6764b70d122810324d45523d8775ab31dc
Kalid Ahamed <admkhalid@zoho.com>
2020-03-12 21:25:16 +0530

    added AWS as media cdn; configs commented for dev

--- osiris_server/settings.py
+++ osiris_server/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'storages',
 ]
 
 # REST_FRAMEWORK = {
@@ -130,3 +131,14 @@ STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+# AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+# AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+# AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+
+# AWS_S3_FILE_OVERWRITE = False
+# # AWS_DEFAULT_ACL = None
+
+# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+# AWS_S3_SIGNATURE_VERSION = "s3v4"
+# AWS_S3_REGION_NAME = "ap-south-1"
