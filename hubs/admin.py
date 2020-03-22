9296b7914a461a1479b3d33b046fa852dfaa1cbb
Kalid Ahamed <admkhalid@zoho.com>
2020-03-22 20:53:44 +0530

    borked all the old shit code

--- hubs/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Hub, Merchant, Product
-
-# Register your models here.
-admin.site.register(Hub)
-admin.site.register(Merchant)
-admin.site.register(Product)
